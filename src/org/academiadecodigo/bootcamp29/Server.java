a6108ac0d4b90ea7b62ac64ade729be36c255b47
Hugo Monteiro <hugoze@netcabo.pt>
2019-06-21 18:40:04 +0100

    added index.html, sockets, listen and processRequest Methods added

--- src/org/academiadecodigo/bootcamp29/Server.java
+++ src/org/academiadecodigo/bootcamp29/Server.java
@@ -1,6 +1,8 @@
 package org.academiadecodigo.bootcamp29;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -19,18 +21,46 @@ public class Server {
         int portNumber = 5005;
 
         try {
-            ServerSocket serverSocket = new ServerSocket();
+            ServerSocket serverSocket = new ServerSocket(portNumber);
 
-            Socket clientSocket = serverSocket.accept();
+            while(true) {
+                Socket clientSocket = serverSocket.accept();
 
-            
+                this.listen(clientSocket);
 
+
+            }
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+
+    }
+
+    private void listen(Socket clientSocket) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String request=in.readLine();
+
+        System.out.println(request);
+
+        this.processRequest(request);
+
+    }
+
+    private void processRequest(String request) {
+
+        String[] splitRequest = request.split(" ");
+
+        for (String component : splitRequest){
+            if (component.contains("/")){
+                
+            }
+
+
+        }
 
     }
 
