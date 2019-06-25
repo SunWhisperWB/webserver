e7c0bf631074f34c128e724a35af4af8bd77977f
Hugo Monteiro <hugoze@netcabo.pt>
2019-06-25 17:32:26 +0100

    server functional everything in main, to be parted, pretty much no website introduced

--- src/org/academiadecodigo/bootcamp29/Server.java
+++ src/org/academiadecodigo/bootcamp29/Server.java
@@ -1,8 +1,6 @@
 package org.academiadecodigo.bootcamp29;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -22,16 +20,11 @@ public class Server {
 
         try {
             ServerSocket serverSocket = new ServerSocket(portNumber);
-
             while(true) {
                 Socket clientSocket = serverSocket.accept();
 
                 this.listen(clientSocket);
-
-
             }
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -40,28 +33,96 @@ public class Server {
     }
 
     private void listen(Socket clientSocket) throws IOException {
+
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         String request=in.readLine();
 
         System.out.println(request);
 
-        this.processRequest(request);
+        this.processRequest(request, clientSocket);
 
     }
 
-    private void processRequest(String request) {
+    private void processRequest(String request, Socket clientSocket) throws IOException {
 
-        String[] splitRequest = request.split(" ");
+        String userRequest;
 
-        for (String component : splitRequest){
-            if (component.contains("/")){
-                
-            }
+        userRequest = request.split(" ")[1];
 
+        switch (userRequest){
+
+            case "/":
+
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                File myFile = new File("www/index.html");
+
+                out.println("HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html; charset=UTF-8\r\n"
+                + "Content-Length" + myFile.length() + "\r\n");
+
+                FileInputStream inputStream = null;
+                DataOutputStream outputStream = null;
+
+                inputStream = new FileInputStream("www/index.html");
+
+                outputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+
+                this.sendFile(inputStream, outputStream);
+
+                out.close();
+
+                break;
+
+            case "/megamanzero.jpg":
+
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                myFile = new File("www/megamanzero.jpg");
+
+                out.println("HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: image/jpg; charset=UTF-8\r\n"
+                        + "Content-Length:" + myFile.length() + "\r\n");
+
+                inputStream = new FileInputStream("www/megamanzero.jpg");
+
+                outputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+
+                this.sendFile(inputStream, outputStream);
+
+                out.close();
+
+                break;
+
+                default:
+                    out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                    out.println("HTTP/1.1 404 Not Found\r\n"
+                            + "Content-Type: image/jpg; charset=UTF-8\r\n"
+                            + "Content-Length:0\r\n");
+
+                    out.close();
+
+                break;
+        }
+    }
+
+    private void sendFile(FileInputStream inputStream, DataOutputStream outputStream) throws IOException {
+
+        byte[] buffer = new byte[1024];
+
+
+        while (inputStream.read(buffer) != -1) {
+
+                outputStream.write(buffer);
 
         }
 
+        outputStream.close();
+        inputStream.close();
     }
 
 }
