48f93724e325753de7d9ed0226a41270a7c59278
Hugo Monteiro <hugoze@netcabo.pt>
2019-06-26 17:54:49 +0100

    added multithreading and said compatibility

--- src/org/academiadecodigo/bootcamp29/Server.java
+++ src/org/academiadecodigo/bootcamp29/Server.java
@@ -20,10 +20,9 @@ public class Server {
 
         try {
             ServerSocket serverSocket = new ServerSocket(portNumber);
-            while(true) {
+            while (true) {
                 Socket clientSocket = serverSocket.accept();
-
-                this.listen(clientSocket);
+                ThreadHandler threadHandler = new ThreadHandler(clientSocket);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,97 +31,6 @@ public class Server {
 
     }
 
-    private void listen(Socket clientSocket) throws IOException {
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        String request=in.readLine();
-
-        System.out.println(request);
-
-        this.processRequest(request, clientSocket);
-
-    }
-
-    private void processRequest(String request, Socket clientSocket) throws IOException {
-
-        String userRequest;
-
-        userRequest = request.split(" ")[1];
-
-        switch (userRequest){
-
-            case "/":
-
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-                File myFile = new File("www/index.html");
-
-                out.println("HTTP/1.1 200 OK\r\n"
-                + "Content-Type: text/html; charset=UTF-8\r\n"
-                + "Content-Length" + myFile.length() + "\r\n");
-
-                FileInputStream inputStream = null;
-                DataOutputStream outputStream = null;
-
-                inputStream = new FileInputStream("www/index.html");
-
-                outputStream = new DataOutputStream(clientSocket.getOutputStream());
-
-
-                this.sendFile(inputStream, outputStream);
-
-                out.close();
-
-                break;
-
-            case "/megamanzero.jpg":
-
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-                myFile = new File("www/megamanzero.jpg");
-
-                out.println("HTTP/1.1 200 OK\r\n"
-                        + "Content-Type: image/jpg; charset=UTF-8\r\n"
-                        + "Content-Length:" + myFile.length() + "\r\n");
-
-                inputStream = new FileInputStream("www/megamanzero.jpg");
-
-                outputStream = new DataOutputStream(clientSocket.getOutputStream());
-
-
-                this.sendFile(inputStream, outputStream);
-
-                out.close();
-
-                break;
-
-                default:
-                    out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-                    out.println("HTTP/1.1 404 Not Found\r\n"
-                            + "Content-Type: image/jpg; charset=UTF-8\r\n"
-                            + "Content-Length:0\r\n");
-
-                    out.close();
-
-                break;
-        }
-    }
-
-    private void sendFile(FileInputStream inputStream, DataOutputStream outputStream) throws IOException {
-
-        byte[] buffer = new byte[1024];
-
-
-        while (inputStream.read(buffer) != -1) {
-
-                outputStream.write(buffer);
-
-        }
-
-        outputStream.close();
-        inputStream.close();
-    }
-
 }
+
+
---
+++ src/org/academiadecodigo/bootcamp29/ThreadHandler.java
@@ -0,0 +1,121 @@
+package org.academiadecodigo.bootcamp29;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ThreadHandler implements Runnable {
+
+    private final Socket clientSocket;
+
+    public ThreadHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+
+        this.listen();
+
+    }
+
+    private void listen() {
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String request = in.readLine();
+
+            System.out.println(request);
+
+            this.processRequest(request);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void processRequest(String request) throws IOException {
+
+        String userRequest;
+
+        userRequest = request.split(" ")[1];
+
+        switch (userRequest) {
+
+            case "/":
+
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                File myFile = new File("www/index.html");
+
+                out.println("HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: text/html; charset=UTF-8\r\n"
+                        + "Content-Length" + myFile.length() + "\r\n");
+
+                FileInputStream inputStream = null;
+                DataOutputStream outputStream = null;
+
+                inputStream = new FileInputStream("www/index.html");
+
+                outputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+
+                this.sendFile(inputStream, outputStream);
+
+                out.close();
+
+                break;
+
+            case "/megamanzero.jpg":
+
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                myFile = new File("www/megamanzero.jpg");
+
+                out.println("HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: image/jpg; charset=UTF-8\r\n"
+                        + "Content-Length:" + myFile.length() + "\r\n");
+
+                inputStream = new FileInputStream("www/megamanzero.jpg");
+
+                outputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+
+                this.sendFile(inputStream, outputStream);
+
+                out.close();
+
+                break;
+
+            default:
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                out.println("HTTP/1.1 404 Not Found\r\n"
+                        + "Content-Type: image/jpg; charset=UTF-8\r\n"
+                        + "Content-Length:0\r\n");
+
+                out.close();
+
+                break;
+        }
+    }
+
+    private void sendFile(FileInputStream inputStream, DataOutputStream outputStream) throws IOException {
+
+        byte[] buffer = new byte[1024];
+
+
+        while (inputStream.read(buffer) != -1) {
+
+            outputStream.write(buffer);
+
+        }
+
+        outputStream.close();
+        inputStream.close();
+    }
+}
+
